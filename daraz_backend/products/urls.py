702d501f605036dbe6268cb99e31d3a90205e083
Mehedi Hassan <168093970+mehedihassan1021@users.noreply.github.com>
2025-04-01 21:05:09 +0600

    Add daraz_backend/products/urls.py 1743519909013

---
+++ daraz_backend/products/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from rest_framework.routers import DefaultRouter
+from .views import ProductViewSet, CategoryViewSet
+
+router = DefaultRouter()
+router.register(r'products', ProductViewSet)
+router.register(r'categories', CategoryViewSet)
+
+urlpatterns = router.urls
