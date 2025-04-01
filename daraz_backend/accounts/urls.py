a4480abc34dbe56c14160b59d6d0148645776971
Mehedi Hassan <168093970+mehedihassan1021@users.noreply.github.com>
2025-04-01 21:04:51 +0600

    Add daraz_backend/accounts/urls.py 1743519891181

---
+++ daraz_backend/accounts/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+from rest_framework_simplejwt.views import TokenRefreshView
+from .views import (
+    RegisterView,
+    UserProfileView,
+    CustomTokenObtainPairView,
+    AddressView
+)
+
+urlpatterns = [
+    path('register/', RegisterView.as_view(), name='register'),
+    path('login/', CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('login/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('profile/', UserProfileView.as_view(), name='user_profile'),
+    path('addresses/', AddressView.as_view(), name='user_addresses'),
+]
